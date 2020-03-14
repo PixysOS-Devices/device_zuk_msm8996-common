48dc373f0c64dad78d064e98d56c4a77b3dc788c
Lucas Dupin <dupin@google.com>
2020-03-14 15:01:44 -0700

    msm8996-common: Enable blurs, under dev-option
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I607ce7de50545b87c3b4d98bb149355c84631e53
    Signed-off-by: Aswin A S <aswinas@pixysos.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -299,3 +299,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Zygote preforking
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.device_config.runtime_native.usap_pool_enabled=true
+
+# Enable blurs, hidden under dev option
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1
+
+# Hint expensive blurs for device with larger display
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.sf.blurs_are_expensive=1
