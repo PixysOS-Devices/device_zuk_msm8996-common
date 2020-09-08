131ce63fff01b3e792303d49047add5d6b7a4714
Aswin A S <aswinas@pixysos.com>
2020-09-08 20:42:40 +0530

    msm8996-common: skip ELF prebuilts check

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -21,6 +21,9 @@ VENDOR_PATH := device/zuk/msm8996-common
 
 TARGET_SPECIFIC_HEADER_PATH := $(VENDOR_PATH)/include
 
+# skip ELF prebuilts check
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
